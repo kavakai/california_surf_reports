7285f236031ee31d2d4fd3b6230823602728843f
Kai Kuller <kavakai@gmail.com>
2023-07-11 11:16:08 -0600

    Add initial tailwind css

--- next.config.js
+++ next.config.js
@@ -2,3 +2,16 @@
 const nextConfig = {}
 
 module.exports = nextConfig
+
+module.exports = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'www.seekpng.com',
+        port: '',
+        pathname: '/png/detail/337-3373081_enso-water-still-shinichi-maruyama.png',
+      },
+    ],
+  },
+}

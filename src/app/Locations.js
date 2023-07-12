b92c54aeea00a96d56e9c1c59e364b7c5cc4bad1
Kai Kuller <kavakai@gmail.com>
2023-07-11 19:14:01 -0600

    Add styling to locations

--- src/app/Locations.js
+++ src/app/Locations.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const Locations = ({spot_name, street_address, id}) => {
   console.log(location, "location")
   return (
-    <div className='container flex justify-center items-center'>
+    <div className='container flex justify-center items-center w-fit hover:scale-110 cursor-pointer'>
       <h1>{spot_name}</h1> 
       <h2>{street_address}</h2>
     </div>
